48dc8e998ad1f92b6bfb7ecc20b6e813b086a214
kuk329 <74480236+kuk329@users.noreply.github.com>
2023-01-11 00:21:08 +0900

    Register page ui 작업

--- exam-student-community/src/App.tsx
+++ exam-student-community/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import Sample from "./Sample";
+import RegisterPage from "./RegisterPage";
+// import SignUp from "./SignUp";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Container}>
+      <RegisterPage />
     </div>
   );
-}
+};
 
 export default App;
---
+++ exam-student-community/src/Sample.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sample = () => {
+  return (
+    <div>
+      <h3>Sample 코드 ...............</h3>
+    </div>
+  );
+};
+
+export default Sample;
