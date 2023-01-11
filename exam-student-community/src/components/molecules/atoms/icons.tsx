1708ba370dadb964e3e23b233438c81c23f75094
kuk329 <74480236+kuk329@users.noreply.github.com>
2023-01-11 20:56:58 +0900

    Register 페이지 수정

--- exam-student-community/src/components/molecules/atoms/icons.tsx
+++ exam-student-community/src/components/molecules/atoms/icons.tsx
@@ -5,10 +5,16 @@ import {
   AiTwotoneLock,
   AiOutlinePoweroff,
 } from "react-icons/ai";
-
+import {
+  MdArrowBackIos,
+  MdMoreVert
+} from "react-icons/md";
 export const IconBar = FaBars;
 export const IconComment = FaRegComment;
 export const IconSearch = IoMdSearch;
 export const IconRarr = AiOutlineRight;
 export const IconLock = AiTwotoneLock;
 export const IconPower = AiOutlinePoweroff;
+// 추가
+export const IconBackBtn = MdArrowBackIos;
+export const IconMoreBtn = MdMoreVert;
