581875d968fb4ba606125069ce8252464409ba40
son426 <coghks0426@naver.com>
2023-02-16 16:11:15 +0900

    mypage 로직 변경 / timer 1차완료 / api 모으기2

--- exam-student-community/src/App.tsx
+++ exam-student-community/src/App.tsx
@@ -3,10 +3,6 @@ import GlobalStyle from "./lib/StyledComponents/GlobalStyle";
 import RouteChangeTracker from "./components/molecules/RouteChangeTracker";
 
 function App() {
-  // RouteChangeTracker();
-  var worker = new Worker("Timer.tsx");
-  worker.postMessage("start");
-
   return (
     <>
       <GlobalStyle />
@@ -16,10 +12,3 @@ function App() {
 }
 
 export default App;
-
-// count state 세팅
-
-// start 버튼 누르면 start_date 기록
-//   setInterval(now_date - start_date diff 계산하고 / count를 diff로 세팅,1초)
-
-// stop 버튼 누르면 end_
